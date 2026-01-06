8810a0894a06651d1fc87ab37a15498057a7c0f1
Juha Kangas <juhakangas5@gmail.com>
2026-01-06 14:13:32 +0200

    feat: integrate Tailwind CSS into playground app

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,13 +1,11 @@
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
-
-import App from "./App";
+import "./globals.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   const root = createRoot(rootElement);
-  root.render(<App />);
+  root.render(<></>);
 } else {
   console.error("Root element not found");
 }
