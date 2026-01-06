13137b9d07e2b0e5b14734bc9f94e06ecc95678a
Juha Kangas <juhakangas5@gmail.com>
2026-01-06 14:34:28 +0200

    feat: implement Home page layout in playground app

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,11 +1,18 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { Home } from "./home";
 
 import "./globals.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   const root = createRoot(rootElement);
-  root.render(<></>);
+  root.render(
+    <StrictMode>
+      <Home />
+    </StrictMode>
+  );
 } else {
   console.error("Root element not found");
 }
