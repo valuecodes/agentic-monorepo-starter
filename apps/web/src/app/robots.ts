fae0b98ebb5e6d10dc5a34962bd58174bc949699
Juha Kangas <juhakangas5@gmail.com>
2026-01-11 08:45:50 +0200

    refactor: remove unused sitemap component and clean up robots file

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -6,7 +6,5 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    // TODO: Uncomment when domain is set
-    // sitemap: "https://example.com/sitemap.xml",
   };
 }
